709aad75981e1727a06a377e1b78dfdba10ce63d
Nakzz <naqibprio10@gmail.com>
2019-08-01 08:08:50 +0000

    Initial build subtree commit

---
+++ build/precache-manifest.9e2017e508e8baa0ac850fe0f4f4e593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000bd6396ba455559833cc262c7e89e3",
+    "url": "/React-Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "2d3536a0ad3f4474f3c6",
+    "url": "/React-Monsters-Rolodex/static/css/main.8cd514c9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/React-Monsters-Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2d3536a0ad3f4474f3c6",
+    "url": "/React-Monsters-Rolodex/static/js/main.51d8b95a.chunk.js"
+  },
+  {
+    "revision": "475910f364a65052b9eb",
+    "url": "/React-Monsters-Rolodex/static/js/runtime~main.88b8c251.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Monsters-Rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
